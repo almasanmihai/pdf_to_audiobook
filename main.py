0a85f35d352186a36924e20cd4f33bed1759b406
almasanmihai <almasanmihai@yahoo.com>
2022-11-22 11:07:53 +0200

    Added file paths and requirements.txt

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pdfplumber
 import PyPDF2
 from tkinter import Tk
 
-Tk().withdraw()
-file = askopenfilename()
+Tk().withdraw()  # no need for entire GUI
+file = askopenfilename()  # file path for choosing file
 
 pdfFileObj = open(file, 'rb')
 
@@ -13,16 +13,18 @@ pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 num_of_pages = pdfReader.numPages
 
+# creating the text
 text_to_say = ''
 with pdfplumber.open(file) as pdf:
     for i in range(0, num_of_pages):
         page = pdf.pages[i]
         text_to_say += page.extract_text()
 
+# audio engine
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-# engine.say(text_to_say)
-filename_to_save = asksaveasfilename() + '.mp3'
-engine.save_to_file(text_to_say, filename_to_save)
+engine.say(text_to_say)
+# filename_to_save = asksaveasfilename() + '.mp3'  # OS file path to save
+# engine.save_to_file(text_to_say, filename_to_save)
 engine.runAndWait()
